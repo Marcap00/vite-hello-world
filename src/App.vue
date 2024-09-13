04e86c88aca3ef9376aa3c95981d05de5276ae11
Marcap00 <cappellatomarco@yahoo.com>
2024-09-13 16:39:15 +0200

    imported AppHero.vue in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
 
 export default {
   components: {
-    AppTitle
+    AppTitle,
+    AppHero
   },
   data() {
     return {
@@ -16,6 +18,7 @@ export default {
 <template>
   <div>
     <AppTitle/>
+    <AppHero/>
   </div>
 </template>
 
